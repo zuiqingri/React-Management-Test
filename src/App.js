4bde62c19092db190d375ca0534a018d5dfea2b8
zuiqingri <72846494+zuiqingri@users.noreply.github.com>
2021-10-01 15:22:18 +0800

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
 
+const customers=[
+  {
+  'id': 1,
+  'image':'https://placeimg.com/64/64/1',
+  'name':'LimGokJong',
+  'birthday':'132456',
+  'gender':'man',
+  'job':'Student'
+},
+{
+  'id': 2,
+  'image':'https://placeimg.com/64/64/2',
+  'name':'LiSunSin',
+  'birthday':'466213',
+  'gender':'man',
+  'job':'Programming'
+},
+{
+  'id': 3,
+  'image':'https://placeimg.com/64/64/3',
+  'name':'HongGilDong',
+  'birthday':'215434',
+  'gender':'man',
+  'job':'Designer'
+}
+]
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system!</h2>
-    </div>
+    <div>
+      {
+      customers.map(a => {
+        return(
+          <Customer
+          key={a.id}
+          id={a.id}
+          image={a.image}
+          name={a.name}
+          birthday={a.birthday}
+          gender={a.gender}
+          job={a.job}
+          />
+        );
+      })
+    }
+  </div>
   );
 }
 
